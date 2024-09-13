b221e3b7e114cac0187452531908cbc25d0e4deb
Shiraz Mazhar <imshiraz007@gmail.com>
2024-09-13 19:10:25 +0500

    Setup Routes

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import {Routes,Route} from "react-router-dom"
+import Home from "./pages/Home.jsx"
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Doctor from './pages/Doctor.jsx'
+import MyAppoinments from './pages/MyAppointments.jsx'
+import MyProfile from './pages/MyProfile.jsx'
+import Login from './pages/Login.jsx'
 const App = () => {
   return (
-    <div>
-      
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes >
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/doctor' element={<Doctor/>}></Route>
+        <Route path='/doctor/:speciality' element={<Doctor/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/myappoinments' element={<MyAppoinments/>}></Route>
+        <Route path='/myappointments' element={<MyAppoinments/>}></Route>
+        <Route path='/myprofile' element={<MyProfile/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+
+      </Routes>
     </div>
   )
 }
