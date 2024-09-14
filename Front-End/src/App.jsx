4f08d8f77ea98afd58ecd4517d92a22808400607
Shiraz Mazhar <imshiraz007@gmail.com>
2024-09-14 20:35:31 +0500

    Test

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -4,20 +4,22 @@ import Home from "./pages/Home.jsx"
 import About from './pages/About.jsx'
 import Contact from './pages/Contact.jsx'
 import Doctor from './pages/Doctor.jsx'
-import MyAppoinments from './pages/MyAppointments.jsx'
 import MyProfile from './pages/MyProfile.jsx'
 import Login from './pages/Login.jsx'
+import MyAppointment from './pages/MyAppointments.jsx'
+import Navbar from './components/Navbar.jsx'
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+     <Navbar/>
       <Routes >
         <Route path='/' element={<Home/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
-        <Route path='/doctor' element={<Doctor/>}></Route>
-        <Route path='/doctor/:speciality' element={<Doctor/>}></Route>
+        <Route path='/doctors' element={<Doctor/>}></Route>
+        <Route path='/doctors/:speciality' element={<Doctor/>}></Route>
         <Route path='/about' element={<About/>}></Route>
-        <Route path='/myappoinments' element={<MyAppoinments/>}></Route>
-        <Route path='/myappointments' element={<MyAppoinments/>}></Route>
+        <Route path='/myappoinments' element={<MyAppointment/>}></Route>
+        <Route path='/myappointments/:docId' element={<MyAppointment/>}></Route>
         <Route path='/myprofile' element={<MyProfile/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/contact' element={<Contact/>}></Route>
