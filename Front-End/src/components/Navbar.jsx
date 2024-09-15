ad39cedbb3f684837638a6b2d39d10e4a902e1fe
Shiraz Mazhar <imshiraz007@gmail.com>
2024-09-15 10:39:20 +0500

    Fix navbar

--- Front-End/src/components/Navbar.jsx
+++ Front-End/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
             <img className='w-8 border-full' src={assets.profile_pic} alt="" />
             <img className='w-2.5' src={assets.dropdown_icon} alt="" />
             <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
-                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-5'>
                     <p onClick={()=>navigate("/myprofile")} className='hover:text-black cursor-pointer'>My Profile</p>
                     <p onClick={()=>navigate("/myappointments")} className='hover:text-black cursor-pointer'>My Appointments</p>
                     <p onClick={()=>setToken(false)}  className='hover:text-black cursor-pointer'>Logout</p>
