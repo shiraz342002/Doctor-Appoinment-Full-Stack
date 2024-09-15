c360a435b6fa9d753c04d06eb34a53761faa5a3e
Shiraz Mazhar <imshiraz007@gmail.com>
2024-09-15 10:48:15 +0500

    Fixed nav

--- Front-End/src/components/Navbar.jsx
+++ Front-End/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import { assets } from "../assets/assets_frontend/assets";
 import { NavLink, useNavigate } from 'react-router-dom';
 const Navbar = () => {
@@ -49,3 +50,5 @@ const Navbar = () => {
   );
 };
 export default Navbar;
+
+
