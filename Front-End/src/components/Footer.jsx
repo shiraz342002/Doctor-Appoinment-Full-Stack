43df5aa8ed780cfb36556e68f644a2520b0f2920
Shiraz Mazhar <imshiraz007@gmail.com>
2024-09-24 11:14:53 +0500

    Contact Page Completed & footer fixed

--- Front-End/src/components/Footer.jsx
+++ Front-End/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className='flex flex-col lg:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-based'>
         <div className='lg:col-span-1'>
           <img src={assets.logo} className='w-56' alt="Logo" />
-          <p className='w-full md:w-2/3 md:font-medium  text-gray-600 leading-6 mt-4'>
+          <p className='w-full md:w-2/4 md:font-medium text-sm md:text-base  text-gray-600 leading-6 mt-4'>
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.          </p>
         </div>
         <div className=''>
--- Front-End/src/pages/Contact.jsx
+++ Front-End/src/pages/Contact.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { assets } from '../assets/assets_frontend/assets'
 
 const Contact = () => {
   return (
-    <div>
-      
+    <div className=''>
+      <div className='text-center text-3xl pt-10 text-gray-500'>
+        <p>Contact <span className='text-gray-700 font-medium'>Us</span></p>
+      </div>
+
+      <div className='flex flex-col md:flex-row gap-10 justify-center mt-10'>
+        <img  className='w-full md:max-w-[360px]'src={assets.contact_image} alt="" />
+        <div className='text-gray-700  flex flex-col mt-5'>
+          <b className='text-lg text-gray-600 mb-4'>OUR OFFICE</b>
+          <p>1540 W Compton Avenue</p>
+          <p>Suite no 340 Los Angeles California</p>
+          <br />
+          <p>Tel: (415) 555‑0132</p>
+          <p>Email: imshiraz007@gmail.com</p>
+          <br />
+          <b className='text-lg text-gray-600 mb-4'>Careers at PRESCRIPTO</b>
+          <p>Learn more about our teams and job openings.</p>
+          <br />
+          <button className='border w-[140px]  py-4 border-black'>Explore Jobs</button>
+        </div>
+      </div>
     </div>
   )
 }
