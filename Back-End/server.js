082a75f34d8afa713bd4a2804c9e4660e9554491
Shiraz Mazhar <imshiraz007@gmail.com>
2024-09-28 12:35:08 +0500

    config & models Created

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,10 +1,16 @@
 import express from "express"
 import cors from "cors"
 import 'dotenv/config'
+import connectCloudinary from "./config/cloudinary.js"
+import connectDB from "./config/mongodb.js"
+
+
 
 //config
 const app = express()
 const port = process.env.PORT || 3000;
+connectDB()
+connectCloudinary()
 
 //middlewares
 app.use(express.json())
