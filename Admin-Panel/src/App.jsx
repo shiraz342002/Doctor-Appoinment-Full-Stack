e57117efb6a36a912e68c634bf3564b52e6b71cb
Shiraz Mazhar <imshiraz007@gmail.com>
2024-09-30 22:05:24 +0500

    Side Bar Created Responsive added

--- Admin-Panel/src/App.jsx
+++ Admin-Panel/src/App.jsx
@@ -4,12 +4,29 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
+import SideBar from './components/SideBar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
 const App = () => {
   const {aToken}=useContext(AdminContext)
   return aToken? (
     <div  className='bg-[#F8F9FD]'>
-      <Navbar/>
       <ToastContainer/>
+      <Navbar/>
+      <div className='flex items-start'>
+        <SideBar/>
+        <Routes>
+          <Route path='/' element={<></>}/>
+          <Route path='/admin-dashboard' element={<Dashboard/>}/>
+          <Route path='/all-appointments' element={<AllAppointments/>}/>
+          <Route path='/add-doctor' element={<AddDoctor/>}/>
+          <Route path='/doctor-list' element={<DoctorsList/>}/>
+
+        </Routes>
+      </div>
     </div>
   ):(
     <>
