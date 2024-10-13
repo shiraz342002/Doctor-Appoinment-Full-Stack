6f7759e07438e15fbee587c2df4ad85cce61db90
Shiraz Mazhar <imshiraz007@gmail.com>
2024-10-13 12:03:45 +0500

    styling for doc profile

--- Admin-Panel/src/pages/Doctor/DoctorProfile.jsx
+++ Admin-Panel/src/pages/Doctor/DoctorProfile.jsx
@@ -3,50 +3,49 @@ import { DoctorContext } from '../../context/DoctorContext'
 import { AppContext } from '../../context/AppContext'
 
 const DoctorProfile = () => {
-  const {dToken,profileData,setProfileData,getDoctorProfile,backendUrl}=useContext(DoctorContext)
-  const {currency}=useContext(AppContext)
+  const { dToken, profileData, setProfileData, getDoctorProfile, backendUrl } = useContext(DoctorContext)
+  const { currency } = useContext(AppContext)
 
-  useEffect(()=>{
-    if(dToken){
+  useEffect(() => {
+    if (dToken) {
       getDoctorProfile()
     }
-  },[dToken])
+  }, [dToken])
   return profileData && (
     <div>
-      <div>
+      <div className='flex flex-col gap-4 m-5'>
         <div>
-          <img src={profileData.image} alt="" />
+          <img className='bg-primary/80 w-full sm:max-w-64 rounded-lg' src={profileData.image} alt="" />
         </div>
-        <div>
+        <div className='flex-1 border border-stone-100 rounded-lg p-8 py-7 bg-white'>
           {/* displaying doc info */}
-          <p>{profileData.name}</p>
-          <div>
-          <p>{profileData.degree} {profileData.speciality}</p>
-          {/* <button>{profileData.experiance}</button> */}
+          <p className='flex items-center gap-2 text-3xl font-medium text-gray-700 '>{profileData.name}</p>
+          <div className='flex items-center gap-2 mt-1 text-gray-600'>
+            <p c>{profileData.degree} {profileData.speciality}</p>
+            {/* <button>{profileData.experiance}</button> */}
           </div>
           {/* doc about */}
           <div>
-            <p>About:</p>
-            <p>{profileData.about}</p>
+            <p className='flex items-center gap-1 text-sm front-medium text-neutral-800 mt-3'>About:</p>
+            <p className='text-sm text-gray-600 max-w-[700px] mt-1'>{profileData.about}</p>
           </div>
+          <p className='text-gray-600 font-medium mt-4'>Appointment fee: <span className='text-gray-800'>{currency} {[profileData.fees]}</span></p>
 
-          <p>Appointment fee: <span>{currency} {[profileData.fees]}</span></p>
-          
-          <div>
+          <div className='flex gap-2 py-2'>
             <p>Address:</p>
-            <p>
-                {profileData.address.line1}
-                 <br />
-                {profileData.address.line2}
+            <p className='text-sm'>
+              {profileData.address.line1}
+              <br />
+              {profileData.address.line2}
             </p>
           </div>
-          <div>
+          <div className='flex gap-1 pt-2'>
             <input type="checkbox" />
             <label htmlFor="">Available</label>
           </div>
-          <button>Edit</button>
-            
-              
+          <button className='px-4 py-1 border border-primary text-sm rounded-full mt-5 '>Edit</button>
+
+
         </div>
       </div>
     </div>
